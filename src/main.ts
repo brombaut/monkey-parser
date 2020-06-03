cfcde5306f46fd8855bf7f88a541f894c1799402
Ben Rombaut <rombaut.benj@gmail.com>
2020-06-03 20:00:54 -0300

    Prettier

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,14 @@ exports.parse = function (input: string): void {
   repl = new REPL();
   repl.start(input);
   if (repl.errors().length > 0) {
-    throw ("Could not parse input");
+    throw "Could not parse input";
   }
-}
+};
 
 exports.ast = function (): string {
   return repl.ast();
-}
+};
 
 exports.errors = function (): string[] {
   return repl.errors();
-}
+};
